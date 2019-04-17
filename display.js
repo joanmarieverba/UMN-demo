2890bda6657810a90dc6547e2515c97324b9ccfa
Joan Marie Verba <verba001@umn.edu>
2019-04-17 15:26:21 -0500

    border button

--- display.js
+++ display.js
@@ -1,2 +1,10 @@
 "use strict"
 
+$(document).ready(function () {
+    $(".borders").click(function () {
+        $(".picture").css({"border": "9px dotted #c905c6", "border-image-source": "linear-gradient(10deg, #fe01f5 0%, #2e113d 100%)", "border-image-slice": "9"});
+    });
+});
+
+
+
